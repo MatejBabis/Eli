12dcbdf76a28c2ab1b86ee3fa50c0b2ce53851bb
MatejBabis <matej@bab.is>
2017-09-26 16:26:21 +0100

    * revert

--- app.py
+++ app.py
@@ -35,18 +35,31 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    # req = request.get_json(silent=True, force=True)
+    #
+    # # Debug:
+    # # print("Request:")
+    # # print(json.dumps(req)
+    #
+    # res = processRequest(req)
+    #
+    # # Debug:
+    # # res = json.dumps(res, indent=4)
+    # # print(res)
+    #
+    # r = make_response(res)
+    # r.headers['Content-Type'] = 'application/json'
+    # return r
+
     req = request.get_json(silent=True, force=True)
 
-    # Debug:
-    # print("Request:")
-    # print(json.dumps(req)
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
     res = processRequest(req)
 
-    # Debug:
-    # res = json.dumps(res, indent=4)
+    res = json.dumps(res, indent=4)
     # print(res)
-
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
@@ -118,6 +131,7 @@ def makeWebhookResult(data):
 
 
 if __name__ == '__main__':
+    # Necessary for Heroku
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
