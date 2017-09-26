2a96fa186b8336c7892eb4e7f04736c02f9ec99c
MatejBabis <matej@bab.is>
2017-09-26 16:00:42 +0100

    * Updated readme; Simplified webhook

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a level 4 project by Matej Babis.
 
 * Webhook: `https://the-eli-bot.herokuapp.com/`
+
+* Inspiration: https://github.com/api-ai/fulfillment-webhook-weather-python
--- app.py
+++ app.py
@@ -37,14 +37,17 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+    # print("Request:")
+    # print(json.dumps(req, indent=4)
 
     res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
-    # print(res)
+    # print("Response:")
+    # print(json.dumps(res, indent=4)
+
+    # Converts the variable to a response object
     r = make_response(res)
+    # Incoming request headers
     r.headers['Content-Type'] = 'application/json'
     return r
 
@@ -115,8 +118,6 @@ def makeWebhookResult(data):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    PORT = 5000
 
-    print("Starting app on port %d" % port)
-
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=PORT, host='0.0.0.0')
